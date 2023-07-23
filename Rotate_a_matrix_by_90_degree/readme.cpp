607d9fd9cbe91eac7ce4ce32127db59adc8b4035
Hariom Rathore <110878349+hariomr@users.noreply.github.com>
2023-07-23 17:25:41 +0530

    readme.cpp

---
+++ Rotate_a_matrix_by_90_degree/readme.cpp
@@ -0,0 +1,33 @@
+## [Rotate a matrix by 90 degree](https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/)
+
+
+### Medium
+
+Given a square matrix, turn it by 90 degrees in a clockwise direction without using any extra space.
+
+## Example 1 :
+
+~~~
+
+Input:
+1 2 3 
+4 5 6
+7 8 9  
+Output:
+7 4 1 
+8 5 2
+9 6 3
+
+
+~~~
+
+### Example 2 :
+
+~~~
+Input:
+1 2
+3 4
+Output:
+3 1
+4 2 
+~~~
